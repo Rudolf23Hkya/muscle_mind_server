84e897f35092591941dc75c5b389e2d5749ef601
Mohnansky Rudolf <rudolf23.hkya@gmail.com>
2024-05-16 18:39:04 +0200

    useless validation removed

--- testing/data_validation.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-#Functions used for API data validation
-    
-def validate_u_experience(experience):
-    if experience not in ["NEW","INTERMEDIATE","EXPERIENCED","PROFESSIONAL"] :
-        raise ValueError("Invalid user Experience value.")
-    
